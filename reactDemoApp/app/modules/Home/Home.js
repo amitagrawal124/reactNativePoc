695d3312c1507d5190e05671d7e9e2e61cce32e2
amitagrawal124 <amit.agrawal@nineleaps.com>
2018-08-11 04:33:24 +0530

    Video listing and play  flow completed

--- reactDemoApp/app/modules/Home/Home.js
+++ reactDemoApp/app/modules/Home/Home.js
@@ -66,7 +66,7 @@ export default class extends React.Component {
                     <Button
                     raised
                     icon={{name: 'cached'}}
-                    title='BUTTON WITH ICON'
+                    title='SUBMIT'
                     backgroundColor="#2a6edc" />
                   </View>
 
--- reactDemoApp/app/modules/ProgramList/ProgramList.js
+++ reactDemoApp/app/modules/ProgramList/ProgramList.js
@@ -1,7 +1,21 @@
 import React from 'react';
-import { StyleSheet, Platform, View, Text, Image, TouchableOpacity, YellowBox } from 'react-native';
+import { StyleSheet, Platform, View, Text, Image, TouchableOpacity, YellowBox, Modal ,TouchableHighlight, WebView} from 'react-native';
+import { Button, Badge ,Icon, FormLabel, FormInput, FormValidationMessage, Card, ListItem, List} from 'react-native-elements';
 
 export default class extends React.Component {
+  state = {
+    modalVisible: false,
+    videoModalVisible : false,
+  };
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
+  setVideoModalVisible(visible) {
+    this.setState({videoModalVisible: visible});
+  }
+
   constructor(props) {
 
         super(props);
@@ -15,12 +29,122 @@ export default class extends React.Component {
 
          render()
          {
+           const list = [
+            {
+              name: 'Amy Farha',
+              avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+              subtitle: 'Vice President'
+            },
+            {
+              name: 'Chris Jackson',
+              avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+              subtitle: 'Vice Chairman'
+            },
+          ]
             return(
+               <View>
+               <View style={{marginTop: 22}}>
+                <Modal
+                  animationType="slide"
+                  transparent={false}
+                  visible={this.state.modalVisible}
+                  onRequestClose={() => {
+                    alert('Modal has been closed.');
+                  }}>
+                  <View style={{marginTop: 22}}>
+                  <View>
 
-               <View style = { styles.MainContainer }>
+                    <TouchableHighlight
+                      onPress={() => {
+                      this.setModalVisible(!this.state.modalVisible);
+                      }}>
+                      <Icon
+                      raised
+                      name='close'
+                      type='font-awesome'
+                      color='#4d4d4d'/>
+                    </TouchableHighlight>
 
-                  <Text style={{fontSize: 23}}> This is Activity - 3 </Text>
+                    <View>
+                      <FormLabel>Video Name</FormLabel>
+                        <FormInput/>
+                      <FormLabel>Description</FormLabel>
+                        <FormInput/>
+                      <FormLabel>Video Url</FormLabel>
+                        <FormInput/>
+                      <Button
+                      raised
+                      icon={{name: 'cached'}}
+                      title='ADD'
+                      backgroundColor="#2a6edc" />
+                    </View>
 
+                  </View>
+                  </View>
+                  </Modal>
+                    <TouchableHighlight
+                      onPress={() => {
+                      this.setModalVisible(true);
+                      }}>
+                      <Badge containerStyle={{ backgroundColor: '#2a6edc'}}>
+                        <Text style={{ color:'#ffffff' }}>Add Video</Text>
+                      </Badge>
+                    </TouchableHighlight>
+                  </View>
+
+                  <View>
+                  <List containerStyle={{marginBottom: 20}}>
+                    {
+                      list.map((l) => (
+                        <ListItem
+                          roundAvatar
+                          avatar={{uri:l.avatar_url}}
+                          key={l.name}
+                          title={l.name}
+                          onPress={() => {
+                          this.setVideoModalVisible(true);
+                          }}
+                        />
+                      ))
+                    }
+                  </List>
+                  </View>
+
+                  <Modal
+                    animationType="slide"
+                    transparent={false}
+                    visible={this.state.videoModalVisible}
+                    onRequestClose={() => {
+                      alert('Modal has been closed.');
+                    }}>
+                    <View style={{marginTop: 22}}>
+                    <View>
+
+                      <TouchableHighlight
+                        onPress={() => {
+                        this.setVideoModalVisible(!this.state.videoModalVisible);
+                        }}>
+                        <Icon
+                        raised
+                        name='close'
+                        type='font-awesome'
+                        color='#4d4d4d'/>
+                      </TouchableHighlight>
+                      <View>
+                      <View style={{ height: 300 }}>
+
+                        <WebView
+                                style={ styles.WebViewContainer }
+                                javaScriptEnabled={true}
+                                domStorageEnabled={true}
+                                source={{uri: 'https://www.youtube.com/embed/dFKhWe2bBkM' }}
+                        />
+
+                          </View>
+                      </View>
+                    </View>
+                    </View>
+                    </Modal>
                </View>
             );
          }
@@ -29,13 +153,10 @@ export default class extends React.Component {
 
 const styles = StyleSheet.create({
 
-MainContainer :{
+WebViewContainer: {
 
-flex:1,
-paddingTop: (Platform.OS) === 'ios' ? 20 : 0,
-alignItems: 'center',
-justifyContent: 'center',
+    marginTop: (Platform.OS == 'ios') ? 20 : 0,
 
-}
+  }
 
 });

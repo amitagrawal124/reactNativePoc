7b8984a0da77789e6e9978b74e8ba8e7108b4bab
amitagrawal124 <amit.agrawal@nineleaps.com>
2018-08-13 15:43:33 +0530

    Integrate event with calendar

--- reactDemoApp/app/modules/Calendar/Calendar.js
+++ reactDemoApp/app/modules/Calendar/Calendar.js
@@ -1,55 +1,93 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   Text,
-  StyleSheet,
-  ScrollView,
-  View
+  View,
+  StyleSheet
 } from 'react-native';
-import {Calendar} from 'react-native-calendars';
+import {Agenda} from 'react-native-calendars';
 
-export default class CalendarsScreen extends Component {
+export default class AgendaScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
-    this.onDayPress = this.onDayPress.bind(this);
+    this.state = {
+      items: {}
+    };
   }
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        <Calendar
-          onDayPress={this.onDayPress}
-          style={styles.calendar}
-          hideExtraDays
-          markedDates={{[this.state.selected]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}}}
+      <View>
+        <Agenda
+          items={this.state.items}
+          loadItemsForMonth={this.loadItems.bind(this)}
+          renderItem={this.renderItem.bind(this)}
+          renderEmptyDate={this.renderEmptyDate.bind(this)}
+          rowHasChanged={this.rowHasChanged.bind(this)}
         />
-      </ScrollView>
+      </View>
     );
   }
 
-  onDayPress(day) {
-    this.setState({
-      selected: day.dateString
-    });
+  loadItems(day) {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = this.timeToString(time);
+        if (!this.state.items[strTime]) {
+          this.state.items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 5);
+          for (let j = 0; j < numItems; j++) {
+            this.state.items[strTime].push({
+              name: 'Item for ' + strTime,
+              height: Math.max(50, Math.floor(Math.random() * 150))
+            });
+          }
+        }
+      }
+      // console.log(this.state.items);
+      const newItems = {};
+      Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
+      this.setState({
+        items: newItems
+      });
+    }, 1000);
+    // console.log(`Load Items for ${day.year}-${day.month}`);
+  }
+
+  renderItem(item) {
+    return (
+      <View style={[styles.item, {height: item.height}]}><Text>{item.name}</Text></View>
+    );
+  }
+
+  renderEmptyDate() {
+    return (
+      <View style={styles.emptyDate}><Text>This is empty date!</Text></View>
+    );
+  }
+
+  rowHasChanged(r1, r2) {
+    return r1.name !== r2.name;
+  }
+
+  timeToString(time) {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
   }
 }
 
 const styles = StyleSheet.create({
-  calendar: {
-    borderTopWidth: 1,
-    paddingTop: 5,
-    borderBottomWidth: 1,
-    borderColor: '#eee',
-    height: 350
-  },
-  text: {
-    textAlign: 'center',
-    borderColor: '#bbb',
-    padding: 10,
-    backgroundColor: '#eee'
-  },
-  container: {
+  item: {
+    backgroundColor: 'white',
     flex: 1,
-    backgroundColor: '#ffffff'
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17
+  },
+  emptyDate: {
+    height: 15,
+    flex:1,
+    paddingTop: 30
   }
 });
